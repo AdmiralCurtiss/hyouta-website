197ea84a925e2fb1135ae8aad28aeb847918b51c
pikachu025 <pikachu025@b666808f-6080-431a-9e40-99878259b3dd>
2013-08-04 03:33:31 +0000

    [No message in converted SVN repo]

--- stream/config.php
+++ stream/config.php
@@ -27,6 +27,7 @@ $apis = array(
 //	"zachnorn" => "Zachnorn",
 	"red13n" => "red13n",
 	"pika025" => "Pika025",
+	"admiralcurtiss" => "Admiral H. Curtiss",
 //	"fuyukiosari" => "Fuyuki",
 	"bitthoven" => "Bitto",
 	"glitchiness" => "Glitchy",
