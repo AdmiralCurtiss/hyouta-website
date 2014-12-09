c9d8245684c9158774294f5c18960cc7d95b4a05
Admiral H. Curtiss <pikachu025@gmail.com>
2014-12-09 14:35:32 +0100

    Vesperia: Enable PS3 data and set it as default.

--- vesperia/index.php
+++ vesperia/index.php
@@ -6,14 +6,12 @@
 
 
 header('Content-Type: text/html; charset=UTF-8');
-$version = '360';
-/*
+$version = 'ps3';
 if ( isset($_GET['version']) ) {
 	if ( $_GET['version'] == 'ps3' || $_GET['version'] == '360' ) {
 		$version = $_GET['version'];
 	}
 }
-*/
 
 require_once 'db.class.php';
 require_once 'header.php';
